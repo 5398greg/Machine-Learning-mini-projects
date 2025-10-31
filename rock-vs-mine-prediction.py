dec6c28d37e343f05c1809fbb40f527f9a218c86
Gregory Kimani <gregkymgreg@gmail.com>
2025-10-31 15:27:42 +0300

    AI/ML mini projects

---
+++ rock-vs-mine-prediction.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+
+sonar_data = pd.read_csv('./data/sonar_data.csv', header=None)
+
+print(sonar_data.head())
+print(sonar_data.shape)
+print(sonar_data[60].value_counts())
+
+# Under Sampling
+mine = sonar_data[sonar_data[60] == 'M']
+print(mine.shape)
+
+mine_sample = mine.sample(n=97)
+print(mine_sample.shape)
+
+
+rock = sonar_data[sonar_data[60] == 'R']
+print(rock.shape)
+
+new_sonar_data = pd.concat([mine_sample, rock], axis=0)
+print(new_sonar_data[60].value_counts())
+
+# Label Encoding
+
+label_encoder = LabelEncoder()
+labels = label_encoder.fit_transform(new_sonar_data[60])
+
+new_sonar_data['Labels'] = labels
+
+print(new_sonar_data.Labels.value_counts())
+
+print(new_sonar_data)
